00aac087945fd5f1ada7f8cbed76b933e5dc457e
Leon Spors <Halfbax@users.noreply.github.com>
2018-03-12 02:43:07 +0100

    Added persistent storage support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,23 @@ wget $(curl -s https://www.teamspeak.com/en/downloads#server | grep -o "http://d
 tar -xjf ./latest.tar.bz2 -C $TS_PATH
 chmod +x ./teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh
 
-if [ -n $LICENSE ]; then
-    if [ $LICENSE = "accept" ]; then
+if [ -n "$LICENSE" ]; then
+    if [ "$LICENSE" = "accept" ]; then
         touch ./teamspeak3-server_linux_amd64/.ts3server_license_accepted
     fi
 fi
 
-./teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh
+if [ -d ./mounted ]; then
+    #if [ ! -d ./mounted/logs ]; then
+    #    mkdir ./mounted/logs
+    #fi
+
+    if [ ! -f /usr/local/teamspeak/teamspeak3-server_linux_amd64/.ts3server_license_accepted -a -f /usr/local/teamspeak/mounted/.ts3server_license_accepted ]; then
+        ln -sf /usr/local/teamspeak/mounted/.ts3server_license_accepted /usr/local/teamspeak/teamspeak3-server_linux_amd64/
+    fi
+    ln -sf /usr/local/teamspeak/mounted/ts3server* /usr/local/teamspeak/teamspeak3-server_linux_amd64/
+
+    startARGS="logpath=/usr/local/teamspeak/mounted/logs licensepath=/usr/local/teamspeak/mounted/"
+fi
+
+./teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh $startARGS
